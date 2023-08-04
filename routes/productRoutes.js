e8b31daaddcda28b4ffabe58a5e67bf6ad2c4f3d
Nick Sheward <nickshwrd@gmail.com>
2023-08-04 13:45:03 +0100

    Further styling applied to password input on edit-product view

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -10,29 +10,17 @@ import {
 import upload from '../services/mutler.js';
 import passwordChecker from '../services/passwordChecker.js';
 
-// Set up an Express router, router has to be used inside an app
 const routes = express.Router();
 
-// Display selected product to view
 routes.get('/product-details/:id', product_get_view);
-
-// Display selected product to edit
 routes.get('/edit-product/:id', product_get_edit);
-
-// Display selected product to delete
 routes.get('/delete-product/:id', product_get_delete);
-
-// Delete product
 routes.delete(
   '/:id',
   passwordChecker(process.env.ADMIN_PASSWORD),
   product_delete
 );
-
-// Add new product to DB
 routes.post('/', upload.single('productImage'), product_post);
-
-// Edit product in DB
 routes.put(
   '/:id',
   passwordChecker(process.env.ADMIN_PASSWORD),
