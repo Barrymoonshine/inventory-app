052df482672702ea913fa185ccdf3c152cbf170e
Nick Sheward <nickshwrd@gmail.com>
2023-08-04 12:05:52 +0100

    Password warning message being shown on edit-product view

--- services/passwordChecker.js
+++ services/passwordChecker.js
@@ -1,12 +1,7 @@
 const passwordChecker = (password) => (req, res, next) => {
-  console.log(
-    'passwordChecker req.headers.authorisation',
-    req.headers.authorisation
-  );
   if (req.headers.authorisation === password) {
     next();
   } else {
-    console.log('failed password check');
     res.status(401).json({ message: 'Unauthorized' });
   }
 };
