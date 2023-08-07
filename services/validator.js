6969c9dfaefb2a159d4a893aa9fd5ad8e640d9c3
Nick Sheward <nickshwrd@gmail.com>
2023-08-07 15:04:22 +0100

    Expanded Express Validator checks to whole add-prod form

--- services/validator.js
+++ services/validator.js
@@ -1,6 +1,29 @@
 import { check, validationResult } from 'express-validator';
 
-const addProdValidationRules = () => [check('name').notEmpty()];
+const addProdValidationRules = () => [
+  check('name').isString().notEmpty().withMessage('Please enter a valid name'),
+  check('sku')
+    .isString()
+    .notEmpty()
+    .withMessage('Please enter a valid SKU reference'),
+  check('description')
+    .isString()
+    .notEmpty()
+    .withMessage('Please enter a valid description'),
+  check('price')
+    .isNumeric()
+    .notEmpty()
+    .custom((value) => value !== 0.0)
+    .withMessage('Price must not be £0.00'),
+  check('quantity')
+    .isNumeric()
+    .notEmpty()
+    .withMessage('Please enter a quantity of 1 or higher'),
+  check('category')
+    .isString()
+    .notEmpty()
+    .withMessage('Please select a value category'),
+];
 
 const validate = (req, res, next) => {
   const errors = validationResult(req);
