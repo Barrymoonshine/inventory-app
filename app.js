fd1e2917ab47cb5f908d755d2b1edba1bdb5385f
Nick Sheward <nickshwrd@gmail.com>
2023-08-09 15:25:21 +0100

    Launched and deployed app onto fly.io, however persisting issue with loading images

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const dbURI = `mongodb+srv://${process.env.DB_CREDENTIALS}@cluster0.wym9xjg.mong
 const connectToDb = async () => {
   try {
     await mongoose.connect(dbURI);
-    app.listen(3000);
+    app.listen(3000, '0.0.0.0', () => {
+      console.log('Server is listening on port 3000');
+    });
   } catch (err) {
     console.log(`Mongoose connection error: ${err}`);
   }
