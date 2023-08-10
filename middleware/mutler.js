fc3d965a91c93b8c39677c9eb127127416faa6d9
Nick Sheward <nickshwrd@gmail.com>
2023-08-10 12:24:31 +0100

    Moved cloudinary config into mutler file

--- middleware/mutler.js
+++ middleware/mutler.js
@@ -1,5 +1,12 @@
 import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const storage = new CloudinaryStorage({
   cloudinary,
@@ -26,19 +33,4 @@ const upload = multer({
   fileFilter,
 });
 
-const dUri = new Datauri();
-
-const parser = new DatauriParser();
-
-const file = parser.format(
-  path.extname(req.file.originalname).toString(),
-  req.file.buffer
-).content;
-
-const dataUri = (req) =>
-  dUri.format(
-    `${req.file.path}`.extname(req.file.originalname).toString(),
-    req.file.buffer
-  );
-
-export { upload, dataUri };
+export default upload;
